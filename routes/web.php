9b800b7fa1c10b7d21d55ca0ef0ff9243ab3ef0d
Tomas <tomascone13@gmail.com>
2022-09-05 03:58:34 -0400

    Posts show detail

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
 
+Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
